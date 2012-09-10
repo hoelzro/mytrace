b001769bb69b0aff8bc7c88c4311bfe4840569c1
Rob Hoelz <rob@hoelz.ro>
2012-09-10 19:30:59 +0200

    Add dumb MySQL connecting script

---
+++ test.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use feature 'say';
+
+use DBI;
+
+my $dbh = DBI->connect('dbi:mysql:host=127.0.0.1', undef, undef, {
+    RaiseError => 1,
+    PrintError => 0,
+});
